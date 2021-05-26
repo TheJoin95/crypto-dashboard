9ef777c8dae41146b852be75ca58592ec66633ac
thejoin95 <miki.lombi@gmail.com>
2021-05-26 15:07:25 +0200

    feat: add compression

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
