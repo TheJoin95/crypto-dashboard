bb63779881367bfbef7a7aa53548f942f2d63f02
thejoin95 <miki.lombi@gmail.com>
2021-05-26 16:55:12 +0200

    feat: add helmet

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   app.use(compression());
   await app.listen(3000);
 }
